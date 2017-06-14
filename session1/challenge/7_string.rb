a6e65c981516e4092eec5c13940a0b51fa40be4a
Sapphire <saphmb@gmail.com>
2017-06-14 13:41:41 +0100

    completed session 1 challenge 7 - string

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,9 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  pry(main)> string.split("r").each {|i| print i[0]}
 end
